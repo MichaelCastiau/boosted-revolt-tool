caa3f34a1142aeb097d4f4d32233d08d46d08c19
Michael Castiau <castiau.m@gmail.com>
2022-06-14 17:56:45 +0200

    Remove ble cause it's shit Update dependencies

---
+++ apps/electron/jest.config.ts
@@ -0,0 +1,15 @@
+/* eslint-disable */
+export default {
+  displayName: 'electron-backend',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/apps/electron/backend',
+};
